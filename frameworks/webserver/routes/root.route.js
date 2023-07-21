2c230e44071f804c409ee67204b7ff338b1968db
Jatin-patil-Furation <jatin.patil@furation.tech>
2023-07-21 12:39:31 +0530

    delete unnecessary files

--- frameworks/webserver/routes/root.route.js
+++ frameworks/webserver/routes/root.route.js
@@ -1,10 +1,8 @@
 // root.route.js
 const express = require("express");
-const BookRouter = require("./book.route");
 const AdminRouter = require("./adminDashboard/admin.route");
 const router = express.Router();
 
-router.use("/book", BookRouter);
 router.use("/admin", AdminRouter);
 
 module.exports = router;
